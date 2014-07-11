a8a8320255c94d40d7a00c78d5819687cee59460
Kun-Yi Li <lankudot@github.com>
2014-07-12 00:44:58 +0800

    ClickNote: Rotate the note by calling transfrom.Rotate

--- ClickNote.cs
+++ ClickNote.cs
@@ -1,5 +1,5 @@
 ﻿/**
- * - Filename: PrepareNote.cs
+ * - Filename: ClickNote.cs
  * - The lifetime of PrepareNote:
  * 1. Sleep after initializing.
  * 2. Sleeping until waked by TapPoint, and then playing the animation.
@@ -15,69 +15,65 @@ using System.Collections;
 
 public class ClickNote : MonoBehaviour
 {
-	public Sprite[] sprites;	// Array storing all frames of prepareNote
+	public Sprite[] sprites;		// Array storing all frames of prepareNote
 	private SpriteRenderer spriteRenderer;
-	private int index;			// The index of frame
-	private int position_ID;	// The index of the position of PrepareNotes
+	private float degreePerFrame;	// The rotation degree of the click note per frame
+	private int rotatingFrames;		// The number of frames in rotating
+	private int delayingFrames;		// The number of frames in delaying
+	private int totalFrames;		// = rotatingFrame + delayingFrame
+	private int index;				// The index of frame
+	private int position_ID;		// The index of the position of PrepareNotes
 
 	// Use this for initialization
 	void Start ()
 	{
 		spriteRenderer = renderer as SpriteRenderer;
-		/* - Calculate the fixedDeltaTime for FixedUpdate().
-		 * - The formula of framesPerSec is
-		 * n * BPM / x / 60,
-		 * which n is the number of frames,
-		 * BPM is the bpm of the song,
-		 * and x is the time interval ( in beats ) for playing
-		 * the entire frames of prepareNotes.
-		 * - fixedDeltaTime ( the interval that FixedUpdate() called,
-		 * and FixedUpdate() would update the frame of PrepareNote ) 
-		 * equals to 1 / framesPerSec.
-		 * - About ( sprites.Length - 5 ):
-		 * Because I added 5 more frames after the best tapping timing, and
-		 * the original frames would be played in 2 beats, calulating the
-		 * fixedDeltaTime must minus 5 ( the # of frames added ).
-		 */
-		// Demo: x = 2, BPM = 140.
-		Time.fixedDeltaTime = ( float ) 60.0f * 2.0f / GameConfig.songBPM / ( sprites.Length - 5 );
+
+		// The click note would rotate 90 degrees in 2 beats
+		rotatingFrames = ( int )GameConfig.framePerBeats * 2;
+		degreePerFrame = 90.0f / ( float )rotatingFrames;
+		// Delaying some frames to avoid the ClickNote disappered suddenly
+		delayingFrames = 10;
+		// Calculate total frames
+		totalFrames = rotatingFrames + delayingFrames;
+
 		/* Initialize the index number of frame by assigning a garbage value to
-		 * avoid grading after initialization. PrepareNote will call OnDisable()
+		 * avoid grading after initialization. The note will call OnDisable()
 		 * which sends the stop frame to Grader, but there is no note assigned
-		 * to TapPoint. The Grader will discard the invaild value.
+		 * to TapPoint at initialization. The Grader will discard the invaild value.
 		 */
-		index = sprites.Length + 10;
+		index = 70;
+
 		// Setting position ID
 		position_ID = gameObject.name[12];
 		position_ID -= 48;
-		// Sleeping after finished initializing.
+
+		spriteRenderer.transform.Rotate( Vector3.back * 90 );
+
+		// Sleeping after initializing.
 		gameObject.SetActive( false );
 	}
-	
-	// FixedUpdate() is called every framerate rate
+
 	void FixedUpdate ()
 	{
-		// Update to next frame.
-		spriteRenderer.sprite = sprites[ index ];
+		spriteRenderer.sprite = sprites[0];
+		// Rotate the click note per frame
+		if ( index < rotatingFrames )
+			spriteRenderer.transform.Rotate( Vector3.forward * degreePerFrame );
+
 		++index;
 		// If the animation ends, sleep.
-		if ( index == sprites.Length )
+		if ( index == totalFrames )
 			gameObject.SetActive( false );
 	}
 
 	// OnDisable() is called when the game object becomes inactive.
 	void OnDisable()
 	{
-		Grader.Instance.grading( position_ID, GameConfig.NoteTypes.CLICK ,index );
-		// Reset index to 0
+		Grader.Instance.grading( position_ID, GameConfig.NoteTypes.CLICK, index );
+		// Reset index to 0 an the angle of the ClickNote
 		index = 0;
-		/* Set the render frame to 0.
-		 * The render of PrepareNote begins before OnEnable() called
-		 * when the PrepareNote becomes active.
-		 * If the reset of the index is in OnEnable(),
-		 * the render would start at the last stop frame.
-		 */
-		spriteRenderer.sprite = sprites[0];
+		spriteRenderer.transform.Rotate( Vector3.back * 90 );
 	}
 
 }	// end of class PrepareNote
