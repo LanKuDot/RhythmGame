5ce7d3722c7023ebaecee2d57b4003a3cee42fdf
Kun-Yi Li <lankudot@github.com>
2014-07-14 00:26:29 +0800

    Remove the reference of judgine frame index

--- GameConfig.cs
+++ GameConfig.cs
@@ -9,7 +9,8 @@ public class GameConfig : MonoBehaviour
 {
 	/* General Setting */
 	public static int numOfTapNotes	= 9;
-	public static float songBPM		= 140f;
+	public static float songBPM		= 140.0f;
+	public static float framePerBeats = 24.0f;
 
 	/* The type of notes */
 	public enum NoteTypes {
@@ -17,21 +18,4 @@ public class GameConfig : MonoBehaviour
 		HOLD
 	};
 
-	/* Sprites */
-	public static int clickNoteSpriteLength = 26;	// Hard coding...
-	public static int holdNoteBeginFrames = 19;
-	public static int holdNoteEndFrames = 18;
-	public static int holdNoteFrames = 37;
-
-	/* Grading */
-	// For click notes: the perfect timing is at frame no. 20
-	public static int click_BAD		= 15;
-	public static int click_EARLY	= 19;
-	public static int click_PERFECT	= 22;
-	public static int click_LATE	= 26;
-	public static int click_MISS	= 26;
-	// For hold notes:
-	public static int hold_BAD		= 26;
-	public static int hold_EARLY	= 32;
-	public static int hold_PERFECT	= 38;
 }	// end of class GameConfig
